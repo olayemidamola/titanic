cf575f20d87d45df939675cf797d3909b8013b6a
Emmanuel Olayemi <damolaolayemi33@gmail.com>
2025-04-05 15:55:40 -0400

    HTML,CSS & JS

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('titanicC.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
